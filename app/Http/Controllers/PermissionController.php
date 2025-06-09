42eaabf17e75ebd8f62d8e1cfbe9d6588d1a24f3
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-06-09 22:53:31 +0530

    refactor(Permission Controller) : optimize code

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -12,13 +12,13 @@ use Yajra\DataTables\Facades\DataTables;
 
 class PermissionController extends Controller implements HasMiddleware
 {
-    public static function middleware():array
+    public static function middleware(): array
     {
         return [
-            new Middleware('permission:view permissions',only:['index']),
-            new Middleware('permission:edit permissions',only:['edit']),
-            new Middleware('permission:create permissions',only:['create']),
-            new Middleware('permission:delete permissions',only:['destroy']),
+            new Middleware('permission:view permissions', only: ['index']),
+            new Middleware('permission:edit permissions', only: ['edit']),
+            new Middleware('permission:create permissions', only: ['create']),
+            new Middleware('permission:delete permissions', only: ['destroy']),
         ];
     }
 
@@ -26,7 +26,7 @@ class PermissionController extends Controller implements HasMiddleware
     {
         return view('permissions.list');
     }
-    
+
     public function data()
     {
         $permissions = Permission::select(['id', 'name', 'created_at']);
@@ -43,59 +43,63 @@ class PermissionController extends Controller implements HasMiddleware
             ->make(true);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('permissions.create');
     }
-    
-    public function store(Request $request){
-        $validator = Validator::make($request->all(),[
-            'name'=>'required|unique:permissions|min:3',
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:permissions|min:3',
         ]);
 
-        if($validator->fails()){
-            if($request->ajax()){
-                session()->flash('error','Something Went Wrong');
+        if ($validator->fails()) {
+            if ($request->ajax()) {
+                session()->flash('error', 'Something Went Wrong');
                 return response()->json([
-                    'errors'=>$validator->errors()
-                ],422);
+                    'errors' => $validator->errors()
+                ], 422);
             }
         }
-            Permission::create(['name'=>$request->name]);
+        Permission::create(['name' => $request->name]);
 
-            if ($request->ajax()) {
-                session()->flash('success','Permission Added Successfully!');
-                return response()->json(['success' => true], 200);
-            }
+        if ($request->ajax()) {
+            session()->flash('success', 'Permission Added Successfully!');
+            return response()->json(['success' => true], 200);
+        }
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $permission = Permission::findOrFail($id);
-        return view('permissions.edit',['permission'=>$permission]);
+        return view('permissions.edit', ['permission' => $permission]);
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $permission = Permission::findOrFail($id);
-        $validator = Validator::make($request->all(),[
-            'name'=>'required|min:3|unique:permissions,name,'.$id.',id',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|unique:permissions,name,' . $id . ',id',
         ]);
 
-        if($validator->fails()){
-            if($request->ajax()){
-                session()->flash('error',$validator->errors());
+        if ($validator->fails()) {
+            if ($request->ajax()) {
+                session()->flash('error', $validator->errors());
                 return response()->json([
-                    'errors'=>$validator->errors(),
-                ],422);
+                    'errors' => $validator->errors(),
+                ], 422);
                 // return redirect()->route('permissions.edit',$id)->withInput()->withErrors($validator);
             }
         }
 
         $permission->name = $request->name;
         $permission->save();
-        session()->flash('success','Permission Updated successfully!');
-        if($request->ajax()){
+        session()->flash('success', 'Permission Updated successfully!');
+        if ($request->ajax()) {
             return response()->json([
-                'success'=> true
-            ],200);
+                'success' => true
+            ], 200);
         }
         // return redirect()->route('permissions.index')->with('success','Permission Updated Successfully!');
 
@@ -108,15 +112,16 @@ class PermissionController extends Controller implements HasMiddleware
         // }
     }
 
-    public function destroy(Request $request){
+    public function destroy(Request $request)
+    {
         $id = $request->id;
 
         $permission = Permission::find($id);
 
-        if($permission == null){
-            session()->flash('error','Permission not found');
+        if ($permission == null) {
+            session()->flash('error', 'Permission not found');
             return response()->json([
-                'status'=>false
+                'status' => false
             ]);
         }
         $permission->delete();
@@ -124,7 +129,7 @@ class PermissionController extends Controller implements HasMiddleware
         session()->flash('success', 'Permission deleted Successfully!');
 
         return response()->json([
-            'status'=>true
+            'status' => true
         ]);
     }
 }
